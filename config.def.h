8ec1c363ff5e558e4ee37e7b2fda6cb7c14c6215
FancyChaos <Felixs.Developer@tutanota.com>
2022-02-06 12:05:09 +0100

    Fixed gaps size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
 /* Gaps and borders */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
